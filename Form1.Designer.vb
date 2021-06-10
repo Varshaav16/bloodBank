8564ba41fa661e8874db6755d1df1ce68cefb5d6
varshaav16 <sec19cs118@sairamtap.edu.in>
2021-06-10 15:36:45 +0530

    Styled buttons

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,23 +20,23 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.btnOk = New System.Windows.Forms.Button()
         Me.txtName = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.Button5 = New System.Windows.Forms.Button()
-        Me.Button6 = New System.Windows.Forms.Button()
-        Me.Button7 = New System.Windows.Forms.Button()
-        Me.Button8 = New System.Windows.Forms.Button()
+        Me.btnOpositive = New System.Windows.Forms.Button()
+        Me.btnOnegative = New System.Windows.Forms.Button()
+        Me.btnABnegative = New System.Windows.Forms.Button()
+        Me.btnABpositive = New System.Windows.Forms.Button()
+        Me.btnAnegative = New System.Windows.Forms.Button()
+        Me.btnBnegative = New System.Windows.Forms.Button()
+        Me.btnBpositive = New System.Windows.Forms.Button()
+        Me.btnApositive = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'btnOk
         '
-        Me.btnOk.Location = New System.Drawing.Point(258, 166)
+        Me.btnOk.Location = New System.Drawing.Point(455, 647)
         Me.btnOk.Name = "btnOk"
         Me.btnOk.Size = New System.Drawing.Size(94, 29)
         Me.btnOk.TabIndex = 0
@@ -45,96 +45,120 @@ Partial Class Form1
         '
         'txtName
         '
-        Me.txtName.Location = New System.Drawing.Point(240, 66)
+        Me.txtName.Location = New System.Drawing.Point(165, 662)
         Me.txtName.Name = "txtName"
         Me.txtName.Size = New System.Drawing.Size(254, 27)
         Me.txtName.TabIndex = 1
         '
-        'Button1
+        'btnOpositive
         '
-        Me.Button1.Location = New System.Drawing.Point(115, 131)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(94, 29)
-        Me.Button1.TabIndex = 2
-        Me.Button1.Text = "O+"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnOpositive.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.btnOpositive.Font = New System.Drawing.Font("Segoe UI", 28.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnOpositive.ForeColor = System.Drawing.Color.Red
+        Me.btnOpositive.Location = New System.Drawing.Point(165, 359)
+        Me.btnOpositive.Name = "btnOpositive"
+        Me.btnOpositive.Size = New System.Drawing.Size(187, 74)
+        Me.btnOpositive.TabIndex = 2
+        Me.btnOpositive.Text = "O+"
+        Me.btnOpositive.UseVisualStyleBackColor = False
         '
-        'Button2
+        'btnOnegative
         '
-        Me.Button2.Location = New System.Drawing.Point(115, 179)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(94, 29)
-        Me.Button2.TabIndex = 3
-        Me.Button2.Text = "A+"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnOnegative.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.btnOnegative.Font = New System.Drawing.Font("Segoe UI", 28.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnOnegative.ForeColor = System.Drawing.Color.Red
+        Me.btnOnegative.Location = New System.Drawing.Point(165, 514)
+        Me.btnOnegative.Name = "btnOnegative"
+        Me.btnOnegative.Size = New System.Drawing.Size(187, 74)
+        Me.btnOnegative.TabIndex = 10
+        Me.btnOnegative.Text = "O-"
+        Me.btnOnegative.UseVisualStyleBackColor = False
         '
-        'Button3
+        'btnABnegative
         '
-        Me.Button3.Location = New System.Drawing.Point(503, 272)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(94, 29)
-        Me.Button3.TabIndex = 4
-        Me.Button3.Text = "B-"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btnABnegative.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.btnABnegative.Font = New System.Drawing.Font("Segoe UI", 28.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnABnegative.ForeColor = System.Drawing.Color.Red
+        Me.btnABnegative.Location = New System.Drawing.Point(1021, 514)
+        Me.btnABnegative.Name = "btnABnegative"
+        Me.btnABnegative.Size = New System.Drawing.Size(187, 74)
+        Me.btnABnegative.TabIndex = 11
+        Me.btnABnegative.Text = "AB-"
+        Me.btnABnegative.UseVisualStyleBackColor = False
         '
-        'Button4
+        'btnABpositive
         '
-        Me.Button4.Location = New System.Drawing.Point(115, 247)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(94, 29)
-        Me.Button4.TabIndex = 5
-        Me.Button4.Text = "B+"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.btnABpositive.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.btnABpositive.Font = New System.Drawing.Font("Segoe UI", 28.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnABpositive.ForeColor = System.Drawing.Color.Red
+        Me.btnABpositive.Location = New System.Drawing.Point(1021, 359)
+        Me.btnABpositive.Name = "btnABpositive"
+        Me.btnABpositive.Size = New System.Drawing.Size(187, 74)
+        Me.btnABpositive.TabIndex = 12
+        Me.btnABpositive.Text = "AB+"
+        Me.btnABpositive.UseVisualStyleBackColor = False
         '
-        'Button5
+        'btnAnegative
         '
-        Me.Button5.Location = New System.Drawing.Point(503, 211)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(94, 29)
-        Me.Button5.TabIndex = 6
-        Me.Button5.Text = "A-"
-        Me.Button5.UseVisualStyleBackColor = True
+        Me.btnAnegative.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.btnAnegative.Font = New System.Drawing.Font("Segoe UI", 28.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnAnegative.ForeColor = System.Drawing.Color.Red
+        Me.btnAnegative.Location = New System.Drawing.Point(446, 514)
+        Me.btnAnegative.Name = "btnAnegative"
+        Me.btnAnegative.Size = New System.Drawing.Size(187, 74)
+        Me.btnAnegative.TabIndex = 13
+        Me.btnAnegative.Text = "A-"
+        Me.btnAnegative.UseVisualStyleBackColor = False
         '
-        'Button6
+        'btnBnegative
         '
-        Me.Button6.Location = New System.Drawing.Point(503, 148)
-        Me.Button6.Name = "Button6"
-        Me.Button6.Size = New System.Drawing.Size(94, 29)
-        Me.Button6.TabIndex = 7
-        Me.Button6.Text = "O-"
-        Me.Button6.UseVisualStyleBackColor = True
+        Me.btnBnegative.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.btnBnegative.Font = New System.Drawing.Font("Segoe UI", 28.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnBnegative.ForeColor = System.Drawing.Color.Red
+        Me.btnBnegative.Location = New System.Drawing.Point(735, 514)
+        Me.btnBnegative.Name = "btnBnegative"
+        Me.btnBnegative.Size = New System.Drawing.Size(187, 74)
+        Me.btnBnegative.TabIndex = 14
+        Me.btnBnegative.Text = "B-"
+        Me.btnBnegative.UseVisualStyleBackColor = False
         '
-        'Button7
+        'btnBpositive
         '
-        Me.Button7.Location = New System.Drawing.Point(503, 343)
-        Me.Button7.Name = "Button7"
-        Me.Button7.Size = New System.Drawing.Size(94, 29)
-        Me.Button7.TabIndex = 8
-        Me.Button7.Text = "AB-"
-        Me.Button7.UseVisualStyleBackColor = True
+        Me.btnBpositive.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.btnBpositive.Font = New System.Drawing.Font("Segoe UI", 28.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnBpositive.ForeColor = System.Drawing.Color.Red
+        Me.btnBpositive.Location = New System.Drawing.Point(735, 359)
+        Me.btnBpositive.Name = "btnBpositive"
+        Me.btnBpositive.Size = New System.Drawing.Size(187, 74)
+        Me.btnBpositive.TabIndex = 15
+        Me.btnBpositive.Text = "B+"
+        Me.btnBpositive.UseVisualStyleBackColor = False
         '
-        'Button8
+        'btnApositive
         '
-        Me.Button8.Location = New System.Drawing.Point(115, 317)
-        Me.Button8.Name = "Button8"
-        Me.Button8.Size = New System.Drawing.Size(94, 29)
-        Me.Button8.TabIndex = 9
-        Me.Button8.Text = "AB+"
-        Me.Button8.UseVisualStyleBackColor = True
+        Me.btnApositive.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.btnApositive.Font = New System.Drawing.Font("Segoe UI", 28.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnApositive.ForeColor = System.Drawing.Color.Red
+        Me.btnApositive.Location = New System.Drawing.Point(446, 359)
+        Me.btnApositive.Name = "btnApositive"
+        Me.btnApositive.Size = New System.Drawing.Size(187, 74)
+        Me.btnApositive.TabIndex = 16
+        Me.btnApositive.Text = "A+"
+        Me.btnApositive.UseVisualStyleBackColor = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button8)
-        Me.Controls.Add(Me.Button7)
-        Me.Controls.Add(Me.Button6)
-        Me.Controls.Add(Me.Button5)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.ClientSize = New System.Drawing.Size(1367, 763)
+        Me.Controls.Add(Me.btnApositive)
+        Me.Controls.Add(Me.btnBpositive)
+        Me.Controls.Add(Me.btnBnegative)
+        Me.Controls.Add(Me.btnAnegative)
+        Me.Controls.Add(Me.btnABpositive)
+        Me.Controls.Add(Me.btnABnegative)
+        Me.Controls.Add(Me.btnOnegative)
+        Me.Controls.Add(Me.btnOpositive)
         Me.Controls.Add(Me.txtName)
         Me.Controls.Add(Me.btnOk)
         Me.Name = "Form1"
@@ -146,12 +170,12 @@ Partial Class Form1
 
     Friend WithEvents btnOk As Button
     Friend WithEvents txtName As TextBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button4 As Button
-    Friend WithEvents Button5 As Button
-    Friend WithEvents Button6 As Button
-    Friend WithEvents Button7 As Button
-    Friend WithEvents Button8 As Button
+    Friend WithEvents btnOpositive As Button
+    Friend WithEvents btnOnegative As Button
+    Friend WithEvents btnABnegative As Button
+    Friend WithEvents btnABpositive As Button
+    Friend WithEvents btnAnegative As Button
+    Friend WithEvents btnBnegative As Button
+    Friend WithEvents btnBpositive As Button
+    Friend WithEvents btnApositive As Button
 End Class
