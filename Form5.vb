a47d4733ef7508d48cc8bb3f28a66ef7395e3351
varshaav16 <sec19cs118@sairamtap.edu.in>
2021-06-13 10:12:58 +0530

    updated

--- Form5.vb
+++ Form5.vb
@@ -2,8 +2,12 @@
 
 
 Public Class Form5
+    'Varshaa - DESKTOP-1D4H6BI\SQLEXPRESS
+    'Sri Vidhya - DESKTOP-TR3PCQ9\SQLEXPRESS
+    'Srindhi -
+    'Sirisha - 
 
-    Dim Connection As New SqlConnection("Server=DESKTOP-TR3PCQ9\SQLEXPRESS; Database=BloodBank; Integrated Security = true")
+    Dim Connection As New SqlConnection("Server=DESKTOP-1D4H6BI\SQLEXPRESS; Database=BloodBank; Integrated Security = true")
 
     Private Sub BtnSave_Click(sender As Object, e As EventArgs) Handles BtnSave.Click
 
@@ -32,9 +36,6 @@ Public Class Form5
 
     End Sub
 
-    Private Sub Form5_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
     Private Sub Form5_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
